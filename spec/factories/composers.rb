b61e099830c817213dc78ad49b861816c1aca57e
Joabe Morais <joabealvesmorais@gmail.com>
2025-05-05 09:29:04 -0300

    config: test the composer model

---
+++ spec/factories/composers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :composer do
+    name { "John Doe" }
+  end
+end
---
+++ spec/models/composer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Composer, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:song_composers).dependent(:destroy) }
+    it { should have_many(:songs).through(:song_composers) }
+  end
+end
